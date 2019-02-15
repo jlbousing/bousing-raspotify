4e5aab0a983d89d929531cf5767b4bbcdf350581
Jorge Luis Bou-saad <jbousing@gmail.com>
2019-02-15 14:54:00 -0400

    added tabs components and new colors in variable.scss and global.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { TabsComponent} from './tabs/tabs.component';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import {TabsComponent} from '../tabs/tabs.component';
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ import { HomePage } from './home.page';
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, TabsComponent]
 })
 export class HomePageModule {}
